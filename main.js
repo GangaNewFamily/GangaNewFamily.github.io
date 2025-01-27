7fc21a52561b121803c299581af59d8e265acb40
Ganga New Family <cerezoluisito@gmail.com>
2025-01-27 18:42:09 -0500

    main.js

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ if (currentPage.includes("index.html")) {
 } else if (currentPage.includes("Proyectos.html")) {
     linkProyectos.classList.add("active");
 }
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+
+// Cierra el menú hamburguesa cuando se hace clic en un enlace del menú
+const menuItems = document.querySelectorAll("#nav a");
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+});
